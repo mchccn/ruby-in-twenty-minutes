5a41e210918e92ed79e52a4b809ae8d36b6d1bdd
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-11-12 10:42:16 -0600

    last line looks like js ngl

---
+++ main.rb
@@ -0,0 +1,32 @@
+"Hello world"
+# => "Hello World"
+
+=begin
+Ruby Obeyed You!
+What just happened?
+Did we just write the world’s shortest “Hello World” program?
+Not exactly.
+The second line is just IRB’s way of telling us the result of the last expression it evaluated.
+If we want to print out “Hello World” we need a bit more:
+=end
+
+puts "Hello world"
+# "Hello world"
+# => nil
+
+=begin
+Your Free Calculator is Here
+Already, we have enough to use IRB as a basic calculator:
+=end
+
+3 + 2
+# => 5
+
+3 * 2
+# => 6
+
+3 ** 3
+# => 9
+
+Math.sqrt(9)
+# => 3.0
